3f09db3579aa0cfb55e9c13be8edc7deb069ed2f
IagoraNz <iago.almeida@ufpi.edu.br>
2023-09-07 20:56:05 -0300

    Atualizacao

--- Apple Iago Store/iago.c
+++ Apple Iago Store/iago.c
@@ -13,9 +13,8 @@ typedef struct produtos {
     char quantidade[tamanho];
 } Produtos;
 
-
-void menufunc(Produtos lista[]){
-    int opc, in, opc1, i = 0;
+void menufunc(Produtos lista[], int *qP){
+    int opc, opc1, i = *qP;
     char prod[tamanho];
 
     printf("_____ MENU DO FUNCIONARIO _____\n");
@@ -24,73 +23,106 @@ void menufunc(Produtos lista[]){
     printf("3 - Estoque\n");
     printf("Opcao desejada: ");
     scanf("%d", &opc);
+    fflush(stdin);
 
-    switch (opc)
+    FILE *pArquivo;
+
+    switch (opc) 
     {
-    case 1:
-        printf("1.1 - Cadastrar\n");
-        printf("1.2 - Armazenados\n");
-        printf("Opcao: ");
-        scanf("%d", &opc1);
+        case 1:
+            printf("1.1 - Cadastrar\n");
+            printf("1.2 - Armazenados\n");
+            printf("1.3 - Voltar\n");
+            printf("Opcao: ");
+            scanf("%d", &opc1);
+            fflush(stdin);
 
-        if(opc1 == 1){
-           printf("Qual categoria de produto? (iPhone, Macbook, iPad, iPods, Acessorios): ");
-           fflush(stdin);
-           scanf(" %s", prod);
+            if(opc1 == 1){
+                printf("Qual categoria de produto? (iPhone, Macbook, iPad, iPod, Acessorios): ");
+                scanf("%s", prod);
+                fflush(stdin);
 
-           const char cat1[] = "iPhone";
+                const char cat1[] = "iPhone";
+                const char cat2[] = "Macbook";
+                const char cat3[] = "iPad";
+                const char cat4[] = "iPod";
+                const char cat5[] = "Acessorios";
 
-           if(strcmp(prod, cat1) == 0){
-                printf("\nNovo iPhone\n");
+                if(strcmp(prod, cat1) == 0 || strcmp(prod, cat2) == 0 || strcmp(prod, cat3) == 0 || strcmp(prod, cat4) == 0 || strcmp(prod, cat5) == 0){
+                    printf("\nNovo Produto\n");
 
-                do{
-                    printf("\nDigite o nome do produto: ");
-                    fflush(stdin);
-                    scanf("%s", lista[i].nome);
+                    do{
+                        printf("\nDigite o codigo do produto: ");
+                        scanf("%s", lista[i].codigo);
+                        fflush(stdin);
+                    }while(strlen(lista[i].codigo) <= 1);
 
-                    in = (strlen(lista[i].nome) <= 1);
+                    do{
+                        printf("\nDigite o nome do produto: ");
+                        scanf("%[^\n]", lista[i].nome);
+                        fflush(stdin);
+                    }while(strlen(lista[i].nome) <= 1);
 
-                    if(in){
-                        printf("Nome nao pode ser vazio! Digite novamente. \n");
+                    do{
+                        printf("\nDigite a quantidade do produto: ");
+                        scanf("%s", lista[i].quantidade);
+                        fflush(stdin);
+                    }while(strlen(lista[i].quantidade) <= 0);
+
+                    (*qP)++;
+
+                    pArquivo = fopen("ArquivoP2.txt", "a");
+
+                    if(pArquivo == NULL){
+                        printf("Erro ao executar o arquivo. \n");
+                        exit(1);
                     }
-                }while(in);
 
-                do{
-                    printf("\nDigite o codigo do produto: ");
-                    fflush(stdin);
-                    scanf("%s", lista[i].nome);
+                    fprintf(pArquivo, "%s\n%s\n%s\n", lista[i].codigo, lista[i].nome, lista[i].quantidade);
+                    fclose(pArquivo);
+                } 
+                else{
+                    printf("Categoria de produto inválida!\n");
+                }
+            } 
+            else if(opc1 == 2){
+                pArquivo = fopen("ArquivoP2.txt", "r");
 
-                    in = (strlen(lista[i].codigo) <= 1);
+                if(pArquivo == NULL){
+                    printf("Dados nao encontrados. Cadastre produtos antes.\n");
+                    break;
+                }
 
-                    if(in){
-                        printf("Codigo nao pode ser vazio! Digite novamente. \n");
-                    }
-                }while(in);
+                system("cls");
 
-                do{
-                    printf("\nDigite a quantidade do produto: ");
-                    fflush(stdin);
-                    scanf(" %s", lista[i].quantidade);
+                printf("_____ ESTOQUE _____\n");
+                printf("CODIGO\tNOME\t\t\tQUANTIDADE\n");
+                for(int j = 0; j <= *qP; j++){
+                    fscanf(pArquivo, "%s", lista[j].codigo);
+                    fscanf(pArquivo, "%s", lista[j].nome);
+                    fscanf(pArquivo, "%s", lista[j].quantidade);
+                    printf("%s\t%-15.15s\t\t%s\n", lista[j].codigo, lista[j].nome, lista[j].quantidade);
+                }
+                fclose(pArquivo);
+            }
+            else if(opc1 == 3){
+                menufunc(lista, qP);
+            }
+            break;
 
-                    in = (strlen(lista[i].quantidade) <= 0);
-
-                    if(in){
-                        printf("Quantidade nao pode ser vazio! Digite novamente. \n");
-                    }
-                }while(in);
-           }
-        }
-        break;
-    
-    default:
-        break;
+        default:
+            break;
     }
+
+    system("PAUSE");
+    menufunc(lista, qP);
 }
 
 void funcionario(){
-    struct produtos lista[tamanho];
+    Produtos lista[tamanho];
     int id;
     char senha[tamanho];
+    int qP = 0;
 
     printf("_____ ACESSO _____\n");
     printf("1 - Cadastrar acesso\n");
@@ -98,29 +130,30 @@ void funcionario(){
     printf("Escolha a opcao desejada: ");
     int login;
     scanf("%d", &login);
-
-    system("cls");
+    fflush(stdin);
 
     FILE *arquivo;
 
     switch (login) 
     {
         case 1:
+            system("cls");
             printf("Bem-vindo ao cadastro do funcionario Apple!\n");
             printf("Digite o seu AppleId (O que voce recebeu no contrato): ");
             scanf("%d", &idA);
+            fflush(stdin);
 
             do{
                 printf("Digite uma senha com no minimo 8 digitos: ");
                 scanf("%s", senhaA);
+                fflush(stdin);
 
                 if(strlen(senhaA) < 8){
                     printf("\nA senha deve ter pelo menos 8 caracteres. Tente novamente.\n\n");
                 } 
-
                 else{
                     arquivo = fopen("cadastrofunc.txt", "w");
-                    
+
                     if(arquivo == NULL){
                         printf("Erro ao executar o arquivo. \n");
                         exit(1);
@@ -131,6 +164,7 @@ void funcionario(){
 
                     printf("Cadastrado com sucesso!\n");
 
+                    system("PAUSE");
                     system("cls");
                     funcionario();
                 }
@@ -144,18 +178,20 @@ void funcionario(){
                 printf("Dados nao encontrados. Cadastra-se antes.\n");
                 break;
             }
-            
+
             fscanf(arquivo, "%d", &idA);
             fscanf(arquivo, "%s", senhaA);
 
             printf("AppleId: ");
             scanf("%d", &id);
+            fflush(stdin);
             printf("Senha: ");
             scanf("%s", senha);
+            fflush(stdin);
 
             if(strcmp(senha, senhaA) == 0 && id == idA){
                 system("cls");
-                menufunc(lista);
+                menufunc(lista, &qP);
             } 
             else{
                 printf("Login ou senha incorretos.\n");
@@ -168,6 +204,8 @@ void funcionario(){
             printf("\nOpcao invalida!\n");
             break;
     }
+
+    system("PAUSE");
 }
 
 void cliente(){
@@ -178,6 +216,7 @@ void cliente(){
     printf("3 - Encontrar produto por palavra-chave\n");
     printf("Escolha a opcao desejada: ");
     scanf("%d", &opc);
+    fflush(stdin);
 
     switch (opc) 
     {
@@ -189,9 +228,10 @@ void cliente(){
             printf("4 - iPods\n");
             printf("5 - Acessorios\n");
             printf("Informe a categoria desejada: ");
-            
+
             int opc1;
             scanf("%d", &opc1);
+            fflush(stdin);
 
             switch (opc1) 
             {
@@ -208,9 +248,12 @@ void cliente(){
             printf("Opcao invalida!\n");
             break;
     }
+
+    system("PAUSE");
 }
 
 int main(){
+
     int opcao;
 
     printf("Bem-vindo na Apple Store!\n");
@@ -218,6 +261,7 @@ int main(){
     printf("Para compras\tPara oficio\n");
     printf("\nSelecione a opcao desejada (1 ou 2): ");
     scanf("%d", &opcao);
+    fflush(stdin);
 
     switch (opcao) 
     {
@@ -227,7 +271,6 @@ int main(){
 
         case 2:
             funcionario();
-            system("PAUSE");
             break;
 
         default:
