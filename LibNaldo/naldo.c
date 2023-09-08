426f337e1a80839415c263476bd41eeb1b7e01ce
FrancinaldoSB <francinaldosousabarbosa21@gmail.com>
2023-09-08 15:39:24 -0300

    new att

--- LibNaldo/naldo.c
+++ LibNaldo/naldo.c
@@ -2,12 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-void menu () {
-    printf("1 - Funcionario\n2 - Cliente\n 3 - Sair");
+int menu (int op) {
+    printf("1 - Funcionario\n2 - Cliente\n3 - Sair\n");
+    printf("Informe sua Escolha: ");
+    scanf("%d", &op);
+    return op;
 }
 
 int main () {
-    printf("Teste");
+    int op;
+    do {
+        system("cls");
+        op = menu(op);
+        switch(op) {
+        case 1:
+        system("cls");
+        printf("Funcionario\n");
+        break;
+        // Funcionario
+        case 2:
+        system("cls");
+        printf("Cliente\n");
+        break;
+        // Cliente
+        case 3:
+        system("cls");
+        printf("Saindo da Aplicacao...\n");
+        break;
+        default:
+        system("cls");
+        printf("Informacao Incorreta!\nInsira um Dado Valido!\n");
+        break;
+        }
+    } while (op != 3);
 
     return 0;
 }
